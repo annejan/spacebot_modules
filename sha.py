f919afff3ea5bb0f42e76be7af9f1c2c4c22682d
Anne Jan Brouwer <brouwer@annejan.com>
2019-07-23 17:46:59 +0200

    Fixed capitalisation and "hack*"

--- sha.py
+++ sha.py
@@ -6,6 +6,6 @@ from willie.module import commands, interval
 @willie.module.example('.sha', 'sha')
 def spacestate(bot, trigger):
   """Gives you a random SHA2021 slogan suggestion"""
-  txt = "for v in s h a; do w=$(grep \"^$v\" /usr/share/dict/cracklib-small | shuf -n1); echo -n \"${w^^${w:0:1}} \"; done ; echo 2021"  
+  txt = "for v in s hack a; do w=$(grep \"^$v\" /usr/share/dict/cracklib-small | shuf -n1); echo -n \"${w^${v:0:1}} \"; done ; echo 2021"  
   bot.say(os.popen(txt).read())
   
